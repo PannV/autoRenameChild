29d7d45848c86a343982c26fbf11de2e9cf2607c
PhanV <phan.visut@gmail.com>
2016-08-31 18:39:58 -0400

    Unity workflow: Rename child object based on parent's name.

---
+++ autoRenameChild.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class autoRenameChild : MonoBehaviour {
+	
+	Transform child;
+	GameObject parent;
+	public string mount = "Mount" ;
+	//var colorNameOnly;
+
+	void Awake(){
+		string parentName = this.gameObject.name;
+		var colorNameOnly = parentName.Remove (parentName.Length - 4);
+
+		//find child that has string "mount"
+		child = transform.FindChild(mount);
+
+		if (child.name != "_" + colorNameOnly + child.name) {
+			//combine parent's name with child's
+			child.name = "_" + colorNameOnly + "Mount"; 
+			mount = child.name; 
+			Debug.Log (parentName + " +mount");
+		} else {
+			Debug.Log ("Already Done:" + parentName + " +mount");
+		}
+
+
+
+	}
+}
